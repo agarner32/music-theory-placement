5e589d5ffad7dd7c8ee5b7589684f9c07c49a992
agarner32 <agarner32@comcast.net>
2026-05-18 17:44:20 -0700

    Update notation.js

--- notation.js
+++ notation.js
@@ -43,6 +43,12 @@ function renderNotation(containerId, config) {
   const eighths = staveNotes.filter(n => n.getDuration() === "8");
   const beams   = eighths.length >= 2 ? [new Beam(eighths)] : [];
 
+  // Add barline after a specific note index if requested
+  if (config.barlineAfter !== undefined) {
+    const { Barline } = Vex.Flow;
+    staveNotes[config.barlineAfter - 1].addModifier(new Barline(Barline.type.SINGLE), 0);
+  }
+
   let numBeats = 4, beatValue = 4;
   if (config.timeSignature) {
     const parts = config.timeSignature.split("/");
